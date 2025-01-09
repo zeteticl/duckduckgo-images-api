db6c9b61b69ca11dc4c40a0b5b51a51c7f9cdf50
Sad <cougar.l@hotmail.com>
2025-01-09 18:24:12 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -20,7 +20,7 @@ async function getToken(keywords) {
         token = res.data.match(/vqd=([\d-]+)\&/)[1]
 
     } catch (error) {
-        console.error('error: duckgo #23')
+        console.error(`error: duckgo #23 Keywords:  ${keywords} \nerror`)
     }
 
     return new Promise((resolve, reject) => {
